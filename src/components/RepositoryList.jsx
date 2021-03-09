7f170007ba4a7573880abf0daa021a3416ee0823
Samanta Próspero <59126841+samantaProspero@users.noreply.github.com>
2021-03-09 14:03:26 -0300

    Conceitos importantes e chamadas http

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { RepositoryItem } from "./RepositoryItem"
+import '../styles/repositories.scss'
+
+// https://api.github.com/users/samantaProspero/repos
+// https://api.github.com/orgs/rocketseat/repos
+
+export function RepositoryList(){
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users/samantaProspero/repos')
+      .then( response =>response.json())
+      .then( data => setRepositories(data))
+  }, [])
+
+  return(
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        {repositories.map( repository => {
+          return <RepositoryItem key={repository.name} repository= {repository} />
+        })}
+      </ul>
+    </section>
+  )
+}
