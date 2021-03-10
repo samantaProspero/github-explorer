18dd05df5c866e61af789d2a08594dcf54a710ee
Samanta Próspero <59126841+samantaProspero@users.noreply.github.com>
2021-03-10 10:12:17 -0300

    usando typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,11 +2,17 @@ import { useEffect, useState } from "react"
 import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 // https://api.github.com/users/samantaProspero/repos
 // https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList(){
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(()=>{
     fetch('https://api.github.com/users/samantaProspero/repos')
